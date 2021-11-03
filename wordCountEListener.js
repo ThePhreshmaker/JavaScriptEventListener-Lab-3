62d8141336e19d25d876d928541caa68581e5c2f
Shawn Philipps <89462917+ThePhreshmaker@users.noreply.github.com>
2021-11-03 09:24:45 -0400

    Add files via upload
    Uploading new files with more comments

--- wordCountEListener.js
+++ wordCountEListener.js
@@ -1,15 +1,21 @@
+/* JavaScript Event Listener to count both Characters(Including Whitespaces) and Words
+ * For Lab 3.3 CISC 3140 Brooklyn College
+ * Author: Shawn P
+*/
+
+//Define variables and get elements from HTML Document
 let userTextArea = document.getElementById("user-textarea");
 let charCount = document.getElementById("char-count");
 let wordCount = document.getElementById("word-count");
 
 userTextArea.addEventListener("input", () => {
   //Counts number of characters
-    charCount.textContent = userTextArea.value.length;
+  charCount.textContent = userTextArea.value.length;
 
   //Removes whitespaces
-  let text = userTextArea.value.trim();
+  let txt = userTextArea.value.trim();
   //Spit text at spaces into array of substrings
   //Filter empty string
   //Count length of this array
-  wordCount.textContent = text.split(/\s+/).filter((item) => item).length;
+  wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
 });
